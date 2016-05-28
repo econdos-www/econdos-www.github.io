dda1b14075baa5ef1d3929a15d3a8ab6f383e87f
Ricardo Paiva <ricardo@ricardopaiva.com>
2016-05-28 17:18:27 -0300

    Update 2016-05-28T20:18:25.346Z

--- js/mail.js
+++ js/mail.js
@@ -2,40 +2,30 @@
   var btnContactUs = $('#btnContactUs');
 
   btnContactUs.on('click', function () {
-    var form, name, email, content;
+    var form, name, email, password, content;
 
     form = $('form[name="contact"]');
 
     name = form.find('input.name');
     email = form.find('input.email');
+    password = form.find('input.password');
 
-    if (name.val() === '' || email.val() === '') {
+    if (name.val() === '' || email.val() === '' || password.val() === '') {
       return swal('Ops...', 'Por favor, para que possamos entrar em contato preencha todos os campos!', 'info');
     }
 
-    content =
-      'Olá, segue abaixo os dados para contato:<br>' +
-      '<br><b>Nome: </b>' + name.val() +
-      '<br><b>E-mail: </b>' + email.val()
-    ;
-
     $.ajax({
       type : "POST",
-      url  : "https://mandrillapp.com/api/1.0/messages/send.json",
+      url  : "https://econdos-backend.herokuapp.com/api/v1/users",
       data : {
-        'key'     : 'e7T7igev5Hu4lhSrsNdOGA',
-        'message' : {
-          'from_email' : 'no-reply@econdos.com.br',
-          'from_name'  : 'eCondos',
-          'to'         : [{'email' : 'contato@econdos.com.br'}],
-          'subject'    : 'eCondos - Tenho Interesse',
-          'html'       : content
-        }
+        name: name.val(),
+        email: email.val(),
+        password: password.val()
       }
     }).success(function (response) {
-      if (response[0].status === 'sent') {
+      if (response._id) {
         swal('Obrigado!', 'Aguarde que logo entraremos em contato!', 'success');
-        name.val(''); email.val('');
+        name.val(''); email.val(''); password.val('');
       }
     });
   });
